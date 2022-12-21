823f7c84d55579479ee919a7297639a47b6820ed
Alexis2207R <104544149+Alexis2207R@users.noreply.github.com>
2022-12-21 09:14:10 -0500

    reporte pdf v1

--- app/Views/include/footer.php
+++ app/Views/include/footer.php
@@ -1,13 +1,11 @@
 <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.2.0
-    </div>
-    <strong>Copyright &copy; 2022 <a target="_blak" href="https://www.desysapp.com">DESYSAPP</a>.</strong> Todos los derechos reservados.
-  </footer>
+</footer>
 
 </div>
 <!-- ./wrapper -->
 
+<!-- jQuery -->
+<script src="<?= base_url()?>/public/plugins/jquery/jquery.min.js"></script>
 
 <!-- Bootstrap 5 -->
 <script src="<?= base_url()?>/public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -20,15 +18,27 @@
 <script src="<?= base_url()?>/public/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
 <script src="<?= base_url()?>/public/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
 <script src="<?= base_url()?>/public/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+
+<script src="<?= base_url()?>/public/plugins/jszip/jszip.min.js"></script>
+<script src="<?= base_url()?>/public/plugins/pdfmake/pdfmake.min.js"></script>
+<script src="<?= base_url()?>/public/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="<?= base_url()?>/public/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="<?= base_url()?>/public/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="<?= base_url()?>/public/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+
 <!-- jquery-validation -->
 <script src="<?= base_url()?>/public/plugins/jquery-validation/jquery.validate.min.js"></script>
 <script src="<?= base_url()?>/public/plugins/jquery-validation/additional-methods.min.js"></script>
+
 <!-- AdminLTE App -->
 <script src="<?= base_url()?>/public/dist/js/adminlte.min.js"></script>
+
 <!-- Bootstrap4 Duallistbox -->
 <script src="<?= base_url()?>/public/plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"></script>
+
 <!-- Select2 -->
 <script src="<?= base_url()?>/public/plugins/select2/js/select2.full.min.js"></script>
+
 <!-- AdminLTE for demo purposes -->
 <script src="<?= base_url()?>/public/dist/js/demo.js"></script>
 <script src="<?= base_url()?>/public/dist/js/sweetalert2.all.min.js"></script>
